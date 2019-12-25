1b7b3d3ea203676daf2b12d2f75e0e5de9fe0fe5
tlu <tlueckow@gmail.com>
2019-12-25 16:55:51 +0100

    Added publish timestamps

--- src/main/java/de/codekeepers/jobcentral/entities/Job.java
+++ src/main/java/de/codekeepers/jobcentral/entities/Job.java
@@ -1,24 +1,37 @@
 package de.codekeepers.jobcentral.entities;
 
+import java.time.LocalDateTime;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.Future;
 import javax.validation.constraints.NotBlank;
+
+import org.springframework.format.annotation.DateTimeFormat;
 
 @Entity
 public class Job {
-    
+
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private long id;
     @NotBlank(message = "Title is mandatory")
     private String title;
-    
+
     @NotBlank(message = "Description is mandatory")
     private String description;
 
-    public Job() {}
+    @DateTimeFormat(pattern = "dd-MM-yyyy HH:mm:ss")
+    @Future
+    private LocalDateTime publishDateTime = LocalDateTime.now().plusMinutes(1);
+
+    @DateTimeFormat(pattern = "dd-MM-yyyy HH:mm:ss")
+    private LocalDateTime publishedTimestamp;
+
+    public Job() {
+    }
 
     public Job(String title, String description) {
         this.title = title;
@@ -28,17 +41,25 @@ public class Job {
     public void setId(long id) {
         this.id = id;
     }
-    
+
     public long getId() {
         return id;
     }
-    
+
     public void setTitle(String title) {
         this.title = title;
     }
-    
+
     public void setDescription(String description) {
         this.description = description;
+    }
+
+    public void setPublishDateTime(LocalDateTime publishDateTime) {
+        this.publishDateTime = publishDateTime;
+    }
+
+    public void setPublishedTimestamp(LocalDateTime publishedTimestamp) {
+        this.publishedTimestamp = publishedTimestamp;
     }
 
     public String getTitle() {
@@ -49,8 +70,21 @@ public class Job {
         return description;
     }
 
+    public LocalDateTime getPublishDateTime() {
+        return publishDateTime;
+    }
+
+    public LocalDateTime getPublishedTimestamp() {
+        return publishedTimestamp;
+    }
+
+    public boolean isPublished() {
+        return publishedTimestamp != null;
+    }
+
     @Override
     public String toString() {
-        return "Job{" + "id=" + id + ", title=" + title + ", description=" + description + '}';
+        return "Job{" + "id=" + id + ", title=" + title + ", description=" + description + ", publishDateTime="
+                + publishDateTime + '}';
     }
 }
